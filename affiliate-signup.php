8df102b4089f196ca7ec9405262c3d531bfb01a7
webmovementllc <webmovementllc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-10 00:31:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/foxypress/tags/0.3.6.1@473015 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliate-signup.php
@@ -0,0 +1,137 @@
+<?php
+/**************************************************************************
+FoxyPress provides a complete shopping cart and inventory management tool 
+for use with FoxyCart's e-commerce solution.
+Copyright (C) 2008-2011 WebMovement, LLC - View License Information - FoxyPress.php
+**************************************************************************/
+
+function foxypress_create_affiliate_signup()
+{
+	global $wpdb, $current_user;
+    get_currentuserinfo();
+    $user_data = get_userdata($current_user->ID); ?>
+
+    <h3>FoxyPress Affiliate Sign Up</h3>
+   	<?php if (isset($_POST['affiliate_signup_submit'])) { 
+    	
+    	$first_name    = foxypress_FixPostVar('affiliate_first_name');
+    	$last_name     = foxypress_FixPostVar('affiliate_last_name');
+    	$facebook_page = foxypress_FixPostVar('affiliate_facebook_page');
+    	$age 		   = foxypress_FixPostVar('affiliate_age');
+    	$gender 	   = foxypress_FixPostVar('affiliate_gender');
+    	$message 	   = foxypress_FixPostVar('affiliate_description');
+    	$avatar_name   = foxypress_FixPostVar('affiliate_avatar_name');
+    	$avatar_ext    = foxypress_FixPostVar('affiliate_avatar_ext');
+    	$error 		   = false;
+
+    	if (empty($first_name)) {
+    		$error = true;
+    		$first_name_error = true;
+    	}
+
+    	if (empty($last_name)) {
+    		$error = true;
+    		$last_name_error = true;
+    	}
+
+    	if (empty($facebook_page)) {
+    		$error = true;
+    		$facebook_page_error = true;
+    	}
+
+    	if (empty($age)) {
+    		$error = true;
+    		$age_error = true;
+    	}
+
+    	if (empty($gender)) {
+    		$error = true;
+    		$gender_error = true;
+    	}
+
+    	if (empty($message)) {
+    		$error = true;
+    		$message_error = true;
+    	}
+
+    	if (!$error) {
+	    	update_user_meta($user_data->ID, 'first_name', $first_name);
+	    	update_user_meta($user_data->ID, 'last_name', $last_name);
+	    	update_user_option($user_data->ID, 'affiliate_facebook_page', $facebook_page);
+	    	update_user_option($user_data->ID, 'affiliate_age', $age);
+	    	update_user_option($user_data->ID, 'affiliate_gender', $gender);
+	    	update_user_meta($user_data->ID, 'description', $message);
+	    	update_user_option($user_data->ID, 'affiliate_avatar_name', $avatar_name);
+	    	update_user_option($user_data->ID, 'affiliate_avatar_ext', $avatar_ext);
+	    	update_user_option($user_data->ID, 'affiliate_user', 'pending'); ?>
+
+	    	<div class="updated" id="message">
+        		<p><strong>Affiliate Request Sent Successfully.</strong></p>
+    		</div>
+
+    	<?php } ?>
+    <?php }
+		$affiliate_user = get_user_option('affiliate_user', $user_id);
+		if ($affiliate_user === 'pending') { ?>
+	   		<div class="updated" id="message">
+	        	<p><strong>Affiliate Request Currently Pending.</strong></p>
+	    	</div>
+	    <?php } else { 
+		    //Multisite specific variables
+		    $user_facebook_page = $wpdb->prefix . 'affiliate_facebook_page'; 
+    		$user_age = $wpdb->prefix . 'affiliate_age';
+    		$user_gender = $wpdb->prefix . 'affiliate_gender'; ?>
+
+		    <script type="text/javascript" language="javascript">
+				jQuery(document).ready(function() {
+					jQuery('#affiliate_gender').val(<?php echo $user_data->$user_gender; ?>);
+				});
+			</script>
+			<form id="affiliate_signup_form" name="affiliate_signup_form" method="POST">
+			<table class="form-table">
+				<tr class="<?php if ($first_name_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_first_name">First Name <span class="description">(required)</span></label></th>
+					<td><input class="regular-text" type="text" name="affiliate_first_name" id="affiliate_first_name" value="<?php echo $user_data->first_name; ?>"><br /></td>
+				</tr>
+				<tr class="<?php if ($last_name_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_last_name">Last Name <span class="description">(required)</span></label></th>
+					<td><input class="regular-text" type="text" name="affiliate_last_name" id="affiliate_last_name" value="<?php echo $user_data->last_name; ?>"></td>
+				</tr>
+				<tr class="<?php if ($avatar_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_avatar">Avatar</label></th>
+					<td>
+						<div id="avatar"></div>
+						<input type="file" name="avatar_upload" id="avatar_upload" value="">
+						<input type="hidden" name="affiliate_avatar_name" id="affiliate_avatar_name" value="">
+						<input type="hidden" name="affiliate_avatar_ext" id="affiliate_avatar_ext" value="">
+					</td>
+				</tr>
+				<tr class="<?php if ($facebook_page_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_facebook_page">Your Facebook Page <span class="description">(required)</span></label></th>
+					<td><input class="regular-text" type="text" name="affiliate_facebook_page" id="affiliate_facebook_page" value="<?php echo $user_data->$user_facebook_page; ?>"></td>
+				</tr>
+				<tr class="<?php if ($age_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_age">Age <span class="description">(required)</span></label></th>
+					<td><input type="text" name="affiliate_age" id="affiliate_age" value="<?php echo $user_data->$user_age; ?>"></td>
+				</tr>
+				<tr class="<?php if ($gender_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_gender">Gender <span class="description">(required)</span></label></th>
+					<td>
+						<select name="affiliate_gender" id="affiliate_gender">
+							<option>Select</option>
+							<option <?php if ($user_data->$user_gender == 'Male') { ?>selected<?php } ?> value="Male">Male</option>
+							<option <?php if ($user_data->$user_gender == 'Female') { ?>selected<?php } ?> value="Female">Female</option>
+						</select>
+					</td>
+				</tr>
+				<tr class="<?php if ($message_error) { ?>form-invalid<?php } ?>">
+					<th><label for="affiliate_description">Message <span class="description">(required)</span></label></th>
+					<td><textarea id="affiliate_description" cols="60" rows="5" name="affiliate_description"><?php echo $user_data->description; ?></textarea><br>
+					<span class="description">Tell us about yourself.</span></td>
+				</tr>
+			</table>
+			<p class="submit"><input type="submit" value="Send Request" class="button-primary" id="affiliate_signup_submit" name="affiliate_signup_submit"></p>
+			</form>
+		<?php }
+	
+}
---
+++ ajax.php
@@ -0,0 +1,144 @@
+<?php
+	/**************************************************************************
+	FoxyPress provides a complete shopping cart and inventory management tool 
+	for use with FoxyCart's e-commerce solution.
+	Copyright (C) 2008-2011 WebMovement, LLC - View License Information - FoxyPress.php
+	**************************************************************************/
+	session_start();
+	$root = dirname(dirname(dirname(dirname(__FILE__))));
+	require_once($root.'/wp-config.php');
+	require_once($root.'/wp-includes/wp-db.php');	
+	global $wpdb;	
+	//global vars
+	$mode = foxypress_FixGetVar('m');	
+	header('Content-type: application/json'); 	
+	if($mode == "tracking")
+	{
+		//tracking vars
+		$lastname = foxypress_FixGetVar('ln');
+		$id = foxypress_FixGetVar('id');	
+		
+		if($id != "" && $lastname != "")
+		{
+			//search table for tracking number
+			$item = $wpdb->get_row("SELECT t.*, s.foxy_transaction_status_description
+									FROM " .  $wpdb->prefix . "foxypress_transaction as t
+									INNER JOIN " . $wpdb->prefix . "foxypress_transaction_status as s on t.foxy_transaction_status = s.foxy_transaction_status
+									WHERE t.foxy_transaction_id = '" . $id . "'
+										and LOWER(t.foxy_transaction_last_name) = LOWER('" . $lastname . "')");
+			if(!empty($item))
+			{
+				$name = $item->foxy_transaction_first_name . " " . $item->foxy_transaction_last_name;
+				$shipping_address = ($item->foxy_transaction_shipping_address1 == "") ? 
+										$item->foxy_transaction_billing_address1 . " " .  $item->foxy_transaction_billing_address2 . "<br/>" .	$item->foxy_transaction_billing_city . ", " . $item->foxy_transaction_billing_state . " " . $item->foxy_transaction_billing_zip . " " . $item->foxy_transaction_billing_country
+										:
+										$item->foxy_transaction_shipping_address1 . " " .  $item->foxy_transaction_shipping_address2 . "<br/>" . $item->foxy_transaction_shipping_city . ", " . $item->foxy_transaction_shipping_state . " " . $item->foxy_transaction_shipping_zip . " " . $item->foxy_transaction_shipping_country;		
+				$status = $item->foxy_transaction_status_description;
+				$tracking = $item->foxy_transaction_trackingnumber;
+				echo("{\"ajax_status\":\"ok\", \"name\": \"" . $name . "\",\"shipping_address\": \"" . $shipping_address . "\",\"current_status\": \"" . $status . "\",\"tracking_number\": \"" . $tracking . "\"}");
+			}
+			else
+			{
+				echo(GetErrorJSON());
+			}
+		}
+		else
+		{
+			echo(GetErrorJSON());
+		}
+	}
+	else if($mode == "deletedownloadable")
+	{
+		//we don't want to delete any downloads in case people still are downloading them.
+		$session_id = foxypress_FixGetVar('sid');
+		$downloadable_id = foxypress_FixGetVar('downloadableid');
+		$inventory_id = foxypress_FixGetVar('inventoryid');		
+		if($session_id == session_id())
+		{
+			if ($downloadable_id != "" && $inventory_id != "") 
+			{
+				$query = "UPDATE ". $wpdb->prefix  . "foxypress_inventory_downloadables SET status = '0' WHERE downloadable_id='" . mysql_escape_string($downloadable_id) . "'";		
+				$wpdb->query($query);			
+			}
+			echo("{\"ajax_status\":\"ok\"}");
+		}
+		else
+		{
+			echo(GetErrorJSON());	
+		}
+	}
+	else if($mode == "savemaxdownloads")
+	{
+		$session_id = foxypress_FixGetVar('sid');
+		$downloadable_id = foxypress_FixGetVar('downloadableid');
+		$inventory_id = foxypress_FixGetVar('inventoryid');		
+		$maxdownloads = foxypress_FixGetVar('maxdownloads');
+		if($session_id == session_id())
+		{
+			if ($downloadable_id != "" && $inventory_id != "") 
+			{			
+				$query = "UPDATE " . $wpdb->prefix  . "foxypress_inventory_downloadables SET maxdownloads='" . $maxdownloads. "' WHERE downloadable_id='" . mysql_escape_string($downloadable_id) . "'";		
+				$wpdb->query($query);		
+				
+			}
+			echo("{\"ajax_status\":\"ok\"}");	
+		}
+		else
+		{
+			echo(GetErrorJSON());	
+		}
+	}
+	else if($mode == "resetdownloadcount")
+	{
+		$session_id = foxypress_FixGetVar('sid');
+		$downloadable_id = foxypress_FixGetVar('downloadableid');
+		$download_transaction_id = foxypress_FixGetVar('downloadtransactionid');		
+		if($session_id == session_id())
+		{
+			if ($downloadable_id != "" && $download_transaction_id != "") 
+			{			
+				$query = "UPDATE " . $wpdb->prefix . "foxypress_downloadable_transaction" . " SET download_count='0' WHERE downloadable_id='" . mysql_escape_string($downloadable_id) . "' AND download_transaction_id='" . mysql_escape_string($download_transaction_id) . "'";		
+				$wpdb->query($query);						
+			}
+			echo("{\"ajax_status\":\"ok\"}");	
+		}
+		else
+		{
+			echo(GetErrorJSON());	
+		}
+	}
+	else if($mode == "save-image-order")
+	{
+		$session_id = foxypress_FixGetVar('sid');
+		if($session_id == session_id())
+		{
+			$imageorder = foxypress_FixGetVar('order');
+			$images = explode(",", $imageorder);
+			$x = 1;
+			foreach($images as $image)
+			{
+				$imageExploded = explode("-", $image);
+				$imageid = $imageExploded[1];
+				$query = "UPDATE " . $wpdb->prefix . "posts SET menu_order='" . $x . "' WHERE ID='" . mysql_escape_string($imageid) . "'";		
+				$wpdb->query($query);
+				$x++;
+			}
+			echo("{\"ajax_status\":\"ok\"}");	
+		}
+		else
+		{
+			echo(GetErrorJSON());	
+		}
+	}
+	else
+	{
+		echo(GetErrorJSON());	
+	}
+	
+	function GetErrorJSON()
+	{
+		return "{\"ajax_status\":\"error\"}";
+	}
+	
+	exit;
+?>
