a9a7766149f0c355c3e4781764016013d96106a9
webmovementllc <webmovementllc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-24 08:02:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/foxypress/tags/0.3.7.1@494507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ foxypress-redirect.php
@@ -0,0 +1,38 @@
+<?php
+/**************************************************************************
+FoxyPress provides a complete shopping cart and inventory management tool 
+for use with FoxyCart's e-commerce solution.
+Copyright (C) 2008-2011 WebMovement, LLC - View License Information - FoxyPress.php
+**************************************************************************/
+
+add_action("template_redirect", 'foxypress_theme_redirect', 1);
+function foxypress_theme_redirect() 
+{
+	global $wp, $wp_query, $currentPageName;
+
+	$currentName = (isset($wp->query_vars["name"]) ? $wp->query_vars["name"] : "");
+	$currentPageName = (isset($wp->query_vars["pagename"]) ? $wp->query_vars["pagename"] : "");
+	$currentPostType = (isset($wp->query_vars["post_type"]) ? $wp->query_vars["post_type"] : "");
+	$currentProduct = (isset($wp->query_vars[FOXYPRESS_CUSTOM_POST_TYPE]) ? $wp->query_vars[FOXYPRESS_CUSTOM_POST_TYPE] : "");	
+	if ($currentPostType == FOXYPRESS_CUSTOM_POST_TYPE && FOXYPRESS_CUSTOM_POST_TYPE != "") 
+	{
+		if (have_posts()) 
+		{
+			if(defined('FOXYPRESS_PRODUCT_TEMPLATE_PATH') && FOXYPRESS_PRODUCT_TEMPLATE_PATH != "")
+			{
+				include FOXYPRESS_PRODUCT_TEMPLATE_PATH;	
+			}
+			else
+			{
+				include FOXYPRESS_PATH . "/themes/foxypress-product.php";
+			}
+			die;
+		} 
+		else 
+		{
+			$wp_query->is_404 = true;
+		}
+	}
+	
+}
+?>
