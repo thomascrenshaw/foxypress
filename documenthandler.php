38d1c4829487ceb40be46cccc25a885b8c2a1d7b
webmovementllc <webmovementllc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-19 15:29:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/foxypress/tags/0.4.2.4@614565 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ documenthandler.php
@@ -0,0 +1,50 @@
+<?php
+/**************************************************************************
+FoxyPress provides a complete shopping cart and inventory management tool 
+for use with FoxyCart's e-commerce solution.
+Copyright (C) 2008-2012 WebMovement, LLC - View License Information - FoxyPress.php
+**************************************************************************/
+
+if ( ! defined( 'ABSPATH' ) ){
+	die( 'Direct access not permitted.' );
+}
+
+global $wpdb;
+
+if (!empty($_FILES)) {
+	$inventory_id = intval( $_POST['inventory_id'] );
+	$downloadabletable = $_POST['prefix'];
+	$downloadablename = $_POST['downloadablename'];
+	$downloadablemaxdownloads = $_POST['downloadablemaxdownloads'];
+	$targetpath = ABSPATH . INVENTORY_DOWNLOADABLE_LOCAL_DIR;
+	$fileExtension = foxypress_ParseFileExtension($_FILES['Filedata']['name']);
+	$prefix = "";
+	if($downloadablename == "")
+	{
+		$prefix = "downloadable_";
+	}
+	else
+	{
+		$prefix = str_replace(" ", "_", $downloadablename);
+		$prefix = $prefix . "_";
+	}	
+	$newfilename = foxypress_GenerateNewFileName($fileExtension, $inventory_id, $targetpath, $prefix);	
+	$targetpath = $targetpath . $newfilename; 	
+	if(move_uploaded_file($_FILES['Filedata']['tmp_name'], $targetpath))
+	{				
+		$query = "INSERT INTO " . $downloadabletable . " SET inventory_id='" . $inventory_id . "', filename='" . mysql_escape_string($newfilename) . "',  maxdownloads= '" . mysql_escape_string($downloadablemaxdownloads) . "', status = 1";
+		$wpdb->query($query);
+		$downloadable_id = $wpdb->insert_id;
+		echo($newfilename . "|" . $downloadable_id . "|" . $downloadablemaxdownloads);				
+	} 
+	else
+	{
+		//failure	
+		esc_html_e('<Error>Error uploading file</Error>', 'foxypress');
+	}		
+}
+else
+{
+	esc_html_e('<Error>Invalid post data</Error>', 'foxypress');	
+}
+?>
---
+++ editor_plugin.js
@@ -0,0 +1,49 @@
+(function() {
+	tinymce.create('tinymce.plugins.FoxyPress', {
+
+		init : function(ed, url) {
+			ed.addCommand('mceFoxyPress', function() {
+				ed.windowManager.open({
+					file : url + '/dialog.php',
+					width : 850 + parseInt(ed.getLang('foxypress.delta_width', 0)),
+					height : 635 + parseInt(ed.getLang('foxypress.delta_height', 0)),
+					inline : 1
+				}, {
+					plugin_url : url, // Plugin absolute URL
+					some_custom_arg : 'custom arg' // Custom argument
+				});
+				
+			});
+
+			ed.addButton('foxypress', {
+				title : 'insert foxycart product',
+				cmd : 'mceFoxyPress',
+				image : url + '/img/wysiwyg.jpg'
+			});
+
+
+			ed.onNodeChange.add(function(ed, cm, n) {
+				cm.setActive('foxypress', n.nodeName == 'IMG');
+			});
+		},
+
+
+		createControl : function(n, cm) {
+			return null;
+		},
+
+		
+		getInfo : function() {
+			return {
+				longname : 'FoxyPress Plugin',
+				author   :  'WebMovement, LLC',
+				authorurl : 'http://www.webmovementllc.com',
+				infourl : 'http://www.webmovementllc.com/foxypress/forum',
+				version : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('foxypress', tinymce.plugins.FoxyPress);
+})();
